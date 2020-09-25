7a7bfb78b28dff56b1c66523cefc0f4f3f2f5b4d
bethea28 <bryanbethea28@gmail.com>
2020-09-24 22:19:42 -0400

    add apollo client cache

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const { ApolloClient, InMemoryCache } = require('@apollo/client')
 const { ApolloServer, gql } = require('apollo-server')
 const { GraphQLScalarType } = require('graphql')
 
 const books = require('./data/books.json')
+const libraries = require('./data/libraries.json')
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+})
 
 const typeDefs = gql`
   scalar DateTime
@@ -20,10 +26,25 @@ const typeDefs = gql`
     status: AvailabilityStatus
   }
 
+  type Library {
+    id: ID
+    name: String
+    address: LibraryAddress
+    phone: String
+  }
+
+  type LibraryAddress {
+    city: String
+    state: String
+    zip: String
+  }
+
   type Query {
     getAllBooks(status: AvailabilityStatus): [Book]
     getBookById(id: String): [Book]
     getBooksByAuthor(author: String): [Book]
+    getLibrary: [Library]
+    getLibraryByName(name: String): [Library]
   }
 
   type SetBookStatusPayload {
@@ -38,42 +59,50 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
+    getLibraryByName: (parent, args) => {
+      console.log('test')
+      //  let final = libraries.filter((a) => a.name.includes(args.name))
+      return libraries.filter((a) => a.name.includes(args.name))
+    },
+    getLibrary: (parent, args) => {
+      return libraries
+    },
     getAllBooks: (parents, args) => {
       console.log('args', args)
       if (!args.status) {
         return books
       } else {
-        return books.filter(book => book.status === args.status)
+        return books.filter((book) => book.status === args.status)
       }
     },
     getBookById: (parent, args) => {
-      let bookById = books.filter(book => book.id === args.id)
+      let bookById = books.filter((book) => book.id === args.id)
       console.log('args', bookById)
 
       return bookById
     },
     getBooksByAuthor: (parent, args) => {
-      let bookList = books.filter(a => a.author === args.author)
+      let bookList = books.filter((a) => a.author === args.author)
       return bookList
-    }
+    },
   },
   Mutation: {
     setBookStatus: (parents, args) => {
       console.log('set', args)
-      let bookStatus = books.find(a => a.id === args.id)
+      let bookStatus = books.find((a) => a.id === args.id)
 
       bookStatus.status = args.status
 
       console.log('books', bookStatus.status)
       return {
         book: bookStatus,
-        changed: new Date()
+        changed: new Date(),
       }
-    }
-  }
+    },
+  },
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, client })
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
