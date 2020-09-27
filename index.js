fc5cc6bc926fe653d9522ec43fd71cf633927899
bethea28 <bryanbethea28@gmail.com>
2020-09-27 17:32:15 -0400

    add subscriptions

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { ApolloClient, InMemoryCache } = require('@apollo/client')
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, PubSub, withFilter } = require('apollo-server')
 const { GraphQLScalarType } = require('graphql')
 
 const books = require('./data/books.json')
 const libraries = require('./data/libraries.json')
+const posts = require('./data/posts.json')
+
+const pubsub = new PubSub()
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -20,7 +23,7 @@ const typeDefs = gql`
     OUT
   }
   type Book {
-    id: String
+    id: ID
     title: String
     author: String
     status: AvailabilityStatus
@@ -40,11 +43,17 @@ const typeDefs = gql`
   }
 
   type Query {
+    posts: [Post]
     getAllBooks(status: AvailabilityStatus): [Book]
     getBookById(id: String): [Book]
     getBooksByAuthor(author: String): [Book]
     getLibrary: [Library]
     getLibraryByName(name: String): [Library]
+  }
+
+  type Post {
+    author: String
+    comment: String
   }
 
   type SetBookStatusPayload {
@@ -55,10 +64,17 @@ const typeDefs = gql`
   type Mutation {
     setBookStatus(id: ID, status: AvailabilityStatus): SetBookStatusPayload
   }
-`
 
+  type Subscription {
+    bookStatusSet: SetBookStatusPayload
+  }
+`
+const BOOK_STATUS_SET = 'BOOK_STATUS_SET'
 const resolvers = {
   Query: {
+    posts: (parent, args) => {
+      return posts
+    },
     getLibraryByName: (parent, args) => {
       console.log('test')
       //  let final = libraries.filter((a) => a.name.includes(args.name))
@@ -88,16 +104,33 @@ const resolvers = {
   },
   Mutation: {
     setBookStatus: (parents, args) => {
-      console.log('set', args)
+      // console.log('set', args)
       let bookStatus = books.find((a) => a.id === args.id)
 
       bookStatus.status = args.status
 
-      console.log('books', bookStatus.status)
+      console.log('BRYAN', bookStatus)
+      // console.log('books', bookStatus.status)
+      pubsub.publish(BOOK_STATUS_SET, {
+        //will hit the bookStatusSet subscrition resolver
+        bookStatusSet: {
+          changed: new Date(),
+          book: bookStatus,
+        },
+      })
       return {
         book: bookStatus,
         changed: new Date(),
       }
+    },
+  },
+  Subscription: {
+    bookStatusSet: {
+      // Additional event labels can be passed to asyncIterator creation
+      subscribe: (payload) => {
+        console.log('payload', payload)
+        return pubsub.asyncIterator([BOOK_STATUS_SET])
+      },
     },
   },
 }
